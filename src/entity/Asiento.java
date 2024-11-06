7fab9b0638221bf0dedfa49c78be82e8402cccea
Alan Gao <1982548075@qq.com>
2024-11-06 19:05:22 -0300

    commit anotacion lomboak

--- src/entity/Asiento.java
+++ src/entity/Asiento.java
@@ -2,16 +2,18 @@ package entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @AllArgsConstructor
+@Getter
 public class Asiento {
 
     private int idAsiento;
     private Zona tipoZona;
+
     private boolean libre;
     private boolean disponible;
-
     public void ocupar(){
         this.libre = false;
     }
