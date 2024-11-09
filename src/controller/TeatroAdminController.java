c3af3c63d4c6d1c04b8cbb65be870905239da814
Tomás <angulo.snaidertomas@gmail.com>
2024-11-09 19:16:23 -0300

    Creacion funciones varias de los controladores

--- src/controller/TeatroAdminController.java
+++ src/controller/TeatroAdminController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import handler.TeatroHandler;
+import view.View;
+
+import java.util.Date;
+
 public class TeatroAdminController {
+
+    private View view;
+
+    public TeatroAdminController(View view) {
+        this.view = view;
+    }
+
+    public void addFuncion(Date fecha, String horaInicio, int duracion, double precio, String nombre) {
+        TeatroHandler.getInstance().addFuncion(fecha, horaInicio, duracion, precio, nombre);
+        view.mostrarMensaje("Función añadida con éxito: " + nombre);
+    }
+
+    public void addGrupo(String nombre, String detalle) {
+        TeatroHandler.getInstance().agregarGrupo(nombre, detalle);
+        view.mostrarMensaje("Grupo añadido con éxito: " + nombre);
+    }
+
+    public void asignarGrupoAFuncion(int idGrupo, int idFuncion) {
+        TeatroHandler.getInstance().asignarGrupoAFuncion(idGrupo, idFuncion);
+        view.mostrarMensaje("Grupo asignado a la función con éxito.");
+    }
+
+    public void mostrarListaTicket() {
+    }
 }
--- src/controller/TeatroUserController.java
+++ src/controller/TeatroUserController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import entity.*;
+import handler.TeatroHandler;
+import view.View;
+
+import java.util.List;
+
 public class TeatroUserController {
+    private View view;
+
+
+    public TeatroUserController(View view) {
+        this.view = view;
+    }
+
+    public void mostrarFunciones() {
+        // Suponemos que TeatroHandler tiene un método getFunciones()
+        List<Funcion> funciones = TeatroHandler.getInstance().getFunciones();
+        view.mostrarFunciones(funciones);
+    }
+
+    public void comprarEntrada(int idFuncion, int idAsiento) {
+        TeatroHandler.getInstance().ocuparAsiento(idAsiento);
+        view.mostrarMensaje("Entrada comprada con éxito para la función " + idFuncion);
+    }
+
+    public void mostrarAsientosLibres(int idFuncion) {
+        List<Asiento> asientosLibres = TeatroHandler.getInstance().asientosLibres(idFuncion);//hace falta ingresar la funcion para ver los asientos disponiblesd e cada funcion? porque en en handler/Teatro no lo pide el metodo como parametro
+        view.mostrarAsientos(asientosLibres);
+    }
 }
