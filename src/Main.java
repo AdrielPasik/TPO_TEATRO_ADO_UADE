1083198269d53d1a19d0329858a0c67862ed5b09
AdrielPasik <adriel.pasik@gmail.com>
2024-11-12 20:23:43 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import entity.TeatroHandler;
 import view.Menu;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! tomassiuuuu");
+
+        cargarDatos();
         Menu.getInstance().mostrarMenuPrincipal();
+    }
+
+    public static void cargarDatos(){
+        TeatroHandler.getInstance().addFuncion(new Date(),"54545",45454,454545,"afafaf");
     }
 }
